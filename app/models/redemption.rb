36438442d2f241898dc7701f50ca662e56df1104
Cosmin Atanasiu <cosmin.atanasiu@li.me>
2025-04-03 21:21:12 +0300

    Updated models & DB with appropriate relationships, validations & callbacks

--- app/models/redemption.rb
+++ app/models/redemption.rb
@@ -4,4 +4,18 @@ class Redemption < ApplicationRecord
 
   validates_presence_of :user
   validates :user_id, uniqueness: { scope: :reward_id, allow_nil: true }
+
+  before_commit :cache_reward_data!, on: :create
+
+  private
+
+  # Cache reward data in JSON format, to allow retrieving it if the Reward is deleted
+  def cache_reward_data!
+    if reward.blank?
+      errors.add(:reward, "must exist")
+      false
+    else
+      self.data = reward.to_json if reward.present?
+    end
+  end
 end
--- db/migrate/20250402230857_create_redemptions.rb
+++ db/migrate/20250402230857_create_redemptions.rb
@@ -2,13 +2,13 @@ class CreateRedemptions < ActiveRecord::Migration[8.0]
   def change
     create_table :redemptions do |t|
       t.references :user, null: false, foreign_key: true
-      t.references :reward, null: false, foreign_key: true
+      t.references :reward, null: true, foreign_key: true
       t.json :data
 
       t.timestamps
     end
 
     # Ensure one redemption per user
-    add_index :redemptions, [ :user_id, :reward_id ], unique: true, where: "post_id IS NOT NULL"
+    add_index :redemptions, [ :user_id, :reward_id ], unique: true, where: "reward_id IS NOT NULL"
   end
 end
